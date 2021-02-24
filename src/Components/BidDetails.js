a6b6f9c533e2aeb585c0cf43e66a70cbe84414c8
Harshad Mane <harshad.mane@pubmatic.com>
2021-02-24 15:00:56 -0800

    removed un-usefull code

--- src/Components/BidDetails.js
+++ src/Components/BidDetails.js
@@ -46,21 +46,6 @@ const useRowStyles = makeStyles({
   }
 });
 
-function createData(name, calories, fat, carbs, protein, price) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    price,
-    history: [
-      { date: '2020-01-05', customerId: '11091700', amount: 3 },
-      { date: '2020-01-02', customerId: 'Anonymous', amount: 1 },
-    ],
-  };
-}
-
 function Row(props) {
   const { row } = props; // row ==> bid
   const [open, setOpen] = React.useState(false);
@@ -141,32 +126,6 @@ function Row(props) {
   );
 };
 
-// Row.propTypes = {
-//   row: PropTypes.shape({
-//     calories: PropTypes.number.isRequired,
-//     carbs: PropTypes.number.isRequired,
-//     fat: PropTypes.number.isRequired,
-//     history: PropTypes.arrayOf(
-//       PropTypes.shape({
-//         amount: PropTypes.number.isRequired,
-//         customerId: PropTypes.string.isRequired,
-//         date: PropTypes.string.isRequired,
-//       }),
-//     ).isRequired,
-//     name: PropTypes.string.isRequired,
-//     price: PropTypes.number.isRequired,
-//     protein: PropTypes.number.isRequired,
-//   }).isRequired,
-// };
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-  createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-  createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-];
-
 const tableClasses = makeStyles({
   root: {
     width: '100%' ,
@@ -177,8 +136,7 @@ const tableClasses = makeStyles({
   }
 });
 
-//  TODO: Remove unnecessary bidderCode
-//  TODO: add PB winning bid status and bid rendered status use Icon
+//  TODO: add PB winning bid status and bid rendered status use Icons with text on hover
 
 export default function BidDetails(props) {
   const classes = tableClasses();
